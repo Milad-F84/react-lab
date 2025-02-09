7ffa061d3fadf23b82ebe13c4464f0c08690f31d
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-02-09 19:49:47 +0330

    project initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Header from './Header.jsx'
+// import Banner from './Banner.jsx';
+// import Describe from './Describe.jsx';
+// import Card from './Card.jsx';
+
+export default function App() {
+  return(
+    <div>
+      <Header />
+    </div>
+  )
+}
