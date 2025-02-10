3f5016510f24ef0af17069d038bdb7ce874a2d73
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-02-10 12:12:05 +0330

    header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from './Header.jsx'
-// import Banner from './Banner.jsx';
+import Banner from './Banner.jsx';
 // import Describe from './Describe.jsx';
 // import Card from './Card.jsx';
 
@@ -8,6 +8,7 @@ export default function App() {
   return(
     <div>
       <Header />
+      <Banner />
     </div>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,2 +1,10 @@
+import airBnbLogo from "./assets/img/airbnb 1.png";
+
+
 export default function Header() {
+    return(
+        <div className="header">
+            <img className="logo" src= {airBnbLogo} alt="" />
+        </div>
+    )
 }
