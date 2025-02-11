0cce56ed5aebd5db7bb34d6c75ce29d0912d2437
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-02-11 19:24:17 +0330

    cards added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Header from './Header.jsx'
 import Banner from './Banner.jsx';
 import Describe from './Describe.jsx';
 import Card from './Card.jsx';
+import card1 from "./assets/img/cards/card1.png";
+import card2 from "./assets/img/cards/card3.png"
+import card3 from "./assets/img/cards/card2.png"
 
 
 const cardData = [
   {
       title: "Life lessons with Katie Zaferes",
-      image: "./assets/img/cards/card1",
+      image: card1,
       rating: 5,
       price: 136,
       count: 6,
@@ -17,7 +20,7 @@ const cardData = [
   },
   {
       title: "Learn wedding photography",
-      image: "./assets/img/cards/card2",
+      image: card2,
       rating: 5,
       price: 125,
       count: 30,
@@ -26,7 +29,7 @@ const cardData = [
   },
   {
       title: "Group Mountain Biking",
-      image: "./assets/img/cards/card3",
+      image: card3,
       rating: 4.8,
       price: 50,
       count: 2,
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,29 +1,23 @@
 import { useState } from "react";
-export default function Card(promps){
-    const {value} = promps;
-     console.log(value)
-     console.log(value.title)
+export default function Card(promps) {
+  const { value } = promps;
 
-    //  const template = value.map(item => {
-    //    <img src={item.image} alt="" />;
-    //    <div>
-    //      <div>
-    //          <p>*</p>
-    //          <p>{item.rating}</p>
-    //          <p>({item.count})</p>
-    //          <p>{item.country}</p>
-    //      </div>
-    //     <p>{item.title}</p>
-    //      <div>
-    //          <p>From {item.price}$</p>
-    //          /
-    //          person
-    //      </div>
-    //    </div>  
-    //  });
-    return(
-        <div>
-        </div>
-    )
+  const  template = value.map((item,index) => (
+    <div className="cards" key={index}>
+      <img className="cardImg" src={item.image} alt="" />
+      <div className="cardContainer">
+        <p className="cardDesc">
+          * {item.rating} {item.count} {item.country}
+        </p>
+        <h3 className="cardTitle">{item.title}</h3>
+        <p className="cardPrice">From {item.price}$ / person</p>
+      </div>
+    </div>
+  ))
 
+  return (
+    <div>
+      {template}
+    </div>
+  );
 }
