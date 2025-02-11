afa1999e13fe14b63bddd9eff15088c057c9e851
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-02-11 17:47:15 +0330

    template create

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,44 @@ import Banner from './Banner.jsx';
 import Describe from './Describe.jsx';
 import Card from './Card.jsx';
 
+
+const cardData = [
+  {
+      title: "Life lessons with Katie Zaferes",
+      image: "./assets/img/cards/card1",
+      rating: 5,
+      price: 136,
+      count: 6,
+      isOnline: false,
+      country: "USA",
+  },
+  {
+      title: "Learn wedding photography",
+      image: "./assets/img/cards/card2",
+      rating: 5,
+      price: 125,
+      count: 30,
+      isOnline: true,
+      country: "USA",
+  },
+  {
+      title: "Group Mountain Biking",
+      image: "./assets/img/cards/card3",
+      rating: 4.8,
+      price: 50,
+      count: 2,
+      isOnline: false,
+      country: "USA",
+  },
+];
+
 export default function App() {
   return(
     <div>
       <Header />
       <Banner />
       <Describe />
-      <Card />
+      <Card value = {cardData}/>
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,33 +1,29 @@
 import { useState } from "react";
-const cardData = [
-    {
-        title: "Life lessons with Katie Zaferes",
-        image: "./assets/img/cards/card1",
-        rating: 5,
-        price: 136,
-        count: 6,
-        isOnline: false,
-        country: "USA",
-    },
-    {
-        title: "Learn wedding photography",
-        image: "./assets/img/cards/card2",
-        rating: 5,
-        price: 125,
-        count: 30,
-        isOnline: true,
-        country: "USA",
-    },
-    {
-        title: "Group Mountain Biking",
-        image: "./assets/img/cards/card3",
-        rating: 4.8,
-        price: 50,
-        count: 2,
-        isOnline: false,
-        country: "USA",
-    },
-];
-export default function Card(){
+export default function Card(promps){
+    const {value} = promps;
+     console.log(value)
+     console.log(value.title)
+
+    //  const template = value.map(item => {
+    //    <img src={item.image} alt="" />;
+    //    <div>
+    //      <div>
+    //          <p>*</p>
+    //          <p>{item.rating}</p>
+    //          <p>({item.count})</p>
+    //          <p>{item.country}</p>
+    //      </div>
+    //     <p>{item.title}</p>
+    //      <div>
+    //          <p>From {item.price}$</p>
+    //          /
+    //          person
+    //      </div>
+    //    </div>  
+    //  });
+    return(
+        <div>
+        </div>
+    )
 
 }
